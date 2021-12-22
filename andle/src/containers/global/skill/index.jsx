59ba449990d2370ace80f43385281a19af2448ca
LWD-LLC <87009143+LWD-LLC@users.noreply.github.com>
2021-12-22 14:08:10 -0500

    initial commit

---
+++ andle/src/containers/global/skill/index.jsx
@@ -0,0 +1,60 @@
+import { Progress } from "reactstrap";
+
+const SkillContainer = () => {
+    return (
+        <div className="section">
+            <div className="container">
+                <div
+                    className="skills-wrapper section-padding wow fadeInUp"
+                    data-wow-delay="0.3s"
+                    data-wow-duration="1.5s"
+                >
+                    <div className="row">
+                        <div className="col-lg-3">
+                            <div className="section-title-2">
+                                <h2 className="title">Skills</h2>
+                            </div>
+                        </div>
+                        <div className="col-lg-9">
+                            <div
+                                className="skill-bar-wrapper"
+                                data-aos="fade-up"
+                                data-aos-duration="1200"
+                            >
+                                <div className="single-skills-bar">
+                                    <h4> Since beginning my tech journey 4 years ago (initially a geology major), I self-taught myself how to use JavaScript, React, React-Native, MERN stack, and more libraries/frameworks.
+                                        I&apos;ve done remote work for agencies, consulted for startups, and collaborated with talented people to create web products for both business and consumer use.
+                                        <br />
+                                        <br />
+                                        I create successful responsive websites that are fast, easy to use, and built with best practices. The main area of my expertise is front-end development, HTML, CSS, JS, building small to large
+                                        web apps, features, animations, and coding interactive layouts.
+                                        <br />
+                                        <br />
+                                        I also have full-stack developer experience with popular open-source CMSs like (WordPress, Sanity.IO, and others).
+                                        <br />
+                                        <br />
+                                        Visit my <a href="https://www.linkedin.com/in/ryan-o-donnell-18aa4a21b/">LinkedIn</a> or just <a href="/contact">contact</a> me.
+                                    </h4>
+                                    <br />
+                                    <br />
+                                    <span className="title">Web Development (MERN Stack, Next.js, JavaScript)</span>
+                                    <br />
+                                    <span className="title">App Development (React-Native)</span>
+                                    <br />
+                                    <span className="title">SEO (Optimization, Integration, Research)</span>
+                                    <br />
+                                    <span className="title">Branding</span>
+                                    <br />
+                                    <span className="title"></span>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SkillContainer;
---
+++ andle/src/layouts/footer/index.jsx
@@ -0,0 +1,68 @@
+const Footer = () => {
+    return (
+        <div className="section bg-dark">
+            <div className="container">
+                <div className="footer-info-box text-center">
+                    <h4 className="title">Let’s work together</h4>
+                    <h2 className="mail">
+                        <a href="mailto:someone@example.com">ryan@limitlessdevs.com</a>
+                    </h2>
+                </div>
+
+                <div className="footer-copyright-social">
+                    <div className="footer-copyright">
+                        <p>
+                            &copy; 2021 <span> Ryan O&apos;Donnell</span>
+                        </p>
+                    </div>
+                    <div className="footer-social">
+                        <ul className="social">
+                            <li>
+                                <a
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className="twitter"
+                                    href="https://twitter.com/LimitlessDev412"
+                                >
+                                    Twitter
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className="behance"
+                                    href="https://www.facebook.com/ryan.odonnell.94043"
+                                >
+                                    Facebook
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className="dribbble"
+                                    href="https://www.instagram.com/limitless_devs412/"
+                                >
+                                    Instagram
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className="github"
+                                    href="https://github.com/LWD-LLC"
+                                >
+                                    Github
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
