0c1c06773c5864463392833afa23534b77007bb4
LWD-LLC <87009143+LWD-LLC@users.noreply.github.com>
2021-12-31 13:01:06 -0500

    Delete andle directory

--- andle/src/Client.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from '@sanity/client';
-
-export default sanityClient({
-    projectId: "bn96cm6n",
-    dataset: "production",
-    apiVersion: '2021-10-08',
-    useCdn: true,
-})
